05ce40400b6fea42c5458c56805da913675ceed1
erickcu <erick.cuenca.cl@gmail.com>
2025-02-06 17:44:26 -0500

    Evapotraspiracion

--- src/app/Experimentos/Evapotraspiracion/page.js
+++ src/app/Experimentos/Evapotraspiracion/page.js
@@ -1,35 +1,34 @@
 'use client';
 import React from 'react';
-import { Link } from 'react-router-dom';
-
+import Link from "next/link";
 
 const LluviaEscorrentia = () => {
   return (
     <div className="home">
       <h2>Evapotraspiracion</h2>
       <div className="buttons">
-        <Link to="/blaney-criddle-global">
+        <Link href="/blaney-criddle-global">
           <button>Blaney Criddle Global</button>
         </Link>
-        <Link to="/blaney-criddle-parcial">
+        <Link href="/blaney-criddle-parcial">
           <button>Blaney Criddle Parcial</button>
         </Link>
-        <Link to="/blaney-criddle-parcial-perenne">
+        <Link href="/blaney-criddle-parcial-perenne">
           <button>Blaney Criddle Parcial Perenne</button>
         </Link>
-        <Link to="/hargreaves">
+        <Link href="/hargreaves">
           <button>Hargreaves</button>
         </Link>
-        <Link to="/penman">
+        <Link href="/penman">
           <button>Penman</button>
         </Link>
-        <Link to="/thorwaite">
+        <Link href="/thorwaite">
           <button>Thorwaite</button>
         </Link>
-        <Link to="/turc">
+        <Link href="/turc">
           <button>Turc</button>
         </Link>
-        <Link to="/balance-hidrico">
+        <Link href="/balance-hidrico">
           <button>Balance hídrico</button>
         </Link>
       </div>
